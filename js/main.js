d202d58ab27dbaddf6ab5dab55bc58831ab76788
Agnieszka Jezierska <xsaminai@gmail.com>
2023-11-22 09:53:53 +0100

    Add year function for footer - JS

---
+++ js/main.js
@@ -0,0 +1,14 @@
+const footerYear = document.querySelector('.footer__year');
+
+
+
+
+const handleCurrentYear = () => {
+	const year = new Date().getFullYear()
+	footerYear.innerText = year
+}
+
+
+
+
+handleCurrentYear();
