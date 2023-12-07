978d7a552b5d7e6123b4309465a7346c05734efe
Agnieszka Jezierska <xsaminai@gmail.com>
2023-12-07 20:07:21 +0100

    Finish nav for mobiles

--- js/main.js
+++ js/main.js
@@ -1,7 +1,18 @@
+const nav = document.querySelector('.nav');
+const navBtn = document.querySelector('.burger-btn');
+const allNavItems = document.querySelectorAll('.nav__item');
 const footerYear = document.querySelector('.footer__year');
 
 
+const handleNav = () => {
+	nav.classList.toggle('nav--active')
 
+	allNavItems.forEach(item => {
+		item.addEventListener('click', () => {
+			nav.classList.remove('nav--active')
+		})
+	})
+}
 
 const handleCurrentYear = () => {
 	const year = new Date().getFullYear()
@@ -12,3 +23,4 @@ const handleCurrentYear = () => {
 
 
 handleCurrentYear();
+navBtn.addEventListener('click', handleNav);
